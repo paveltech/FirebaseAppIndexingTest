08743364a53b00fe8ffd62b4e0948aaee30c6e02
pavelcep.sust11@gmail.com <pavelandroid11>
2017-09-23 17:06:48 +0600

    Initial commit

---
+++ app/src/main/java/com/playofffstudio/hindivideosongs/Songs.java
@@ -0,0 +1,46 @@
+package com.playofffstudio.hindivideosongs;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class Songs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_songs);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        onNewIntent(getIntent());
+    }
+
+
+    protected void onNewIntent(Intent intent) {
+        String action = intent.getAction();
+        String data = intent.getDataString();
+        if (Intent.ACTION_VIEW.equals(action) && data != null) {
+            String recipeId = data.substring(data.lastIndexOf("/") + 1);
+            Uri contentUri = Uri.parse("content://bollywoodgaana.com/songs").buildUpon()
+                    .appendPath(recipeId).build();
+            Toast.makeText(this, ""+contentUri, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
